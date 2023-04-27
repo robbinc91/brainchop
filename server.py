12171ded3c12644d1c80fa21f600b8e4c0836f38
robbinc91 <robbinc91@gmail.com>
2023-04-27 03:51:38 -0400

    successfully sending files to backend

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, redirect
 from backend import Predictor
+from werkzeug.utils import secure_filename
+import os
 
 
 predictor = Predictor('backend/models')
 
+UPLOAD_FOLDER = './temp'
+ALLOWED_EXTENSIONS = {'nii.gz'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
@@ -24,3 +30,36 @@ def index():
     for model in models:
         print(model['name'])
     return render_template('index.html', available_models=models)
+
+
+def allowed_file(filename):
+    return True
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def process_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        print(f'Received file {file.filename} for processing')
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            if not os.path.exists(app.config['UPLOAD_FOLDER']):
+                os.makedirs(app.config['UPLOAD_FOLDER'])
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return filename
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
